3dcaf9413781f40d00a8cf127ed274d5aa6b7d3c
Gary FELICITE-PRUDENT <gary.felicite-prudent@teladoc.com>
2021-09-10 13:09:22 +0200

    add express

--- index.js
+++ index.js
@@ -1,19 +1,10 @@
-var html_to_pdf = require('html-pdf-node');
-const fs = require('fs-extra');
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 3000
 
-let options = { format: 'A4' };
-// Example of options with args //
-// let options = { format: 'A4', args: ['--no-sandbox', '--disable-setuid-sandbox'] };
-
-//let file = { content: "<h1>Welcome to html-pdf-node</h1>" };
-// or //
-let file = { url: "https://www.npmjs.com/package/html-pdf-node" };
-
-function callbackFunction() {
-    console.log("done")
-}
-
-html_to_pdf.generatePdf(file, options).then(pdfBuffer => {
-    fs.writeFile('file.pdf', pdfBuffer, callbackFunction)
-});
-
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`))
