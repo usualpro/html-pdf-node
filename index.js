424ac31e3f10de25a7791fc1e2e6bbefbe9b3948
Gary FELICITE-PRUDENT <gary.felicite-prudent@teladoc.com>
2021-09-10 13:30:46 +0200

    test pdf generation

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
+const html_to_pdf = require('html-pdf-node');
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 3000
+const options = { format: 'A4' };
 
 express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${PORT}`))
+    .use(express.static(path.join(__dirname, 'public')))
+    .set('views', path.join(__dirname, 'views'))
+    .set('view engine', 'ejs')
+    .get('/', (req, res) => res.render('pages/index'))
+    .post('/generate', (req, res) => {
+        const { url } = req.body
+        html_to_pdf.generatePdf(url, options).then(pdfBuffer => {
+            return pdfBuffer
+        })
+    })
+    .listen(PORT, () => console.log(`Listening on ${PORT}`))
