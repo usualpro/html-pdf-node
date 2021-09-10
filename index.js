c3877d0ff2c2af209e8ec7c5aeaa03a0ebd5beae
Gary FELICITE-PRUDENT <gary.felicite-prudent@teladoc.com>
2021-09-10 17:17:15 +0200

    u

--- index.js
+++ index.js
@@ -4,7 +4,13 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 3000
 const options = { format: 'A4' };
-
+const generate = async (url) => {
+    return await html_to_pdf.generatePdf("<h1>qsdqsdqsd</h1>", options).then(pdfBuffer => {
+        return pdfBuffer
+    }).catch(error => {
+        console.log(error);
+    })
+}
 express()
     .use(bodyParser.json())
     .use(bodyParser.urlencoded({ extended: true }))
@@ -14,10 +20,6 @@ express()
     .get('/', (req, res) => res.render('pages/index'))
     .post('/generate', (req, res) => {
         const { html } = req.body
-        html_to_pdf.generatePdf("<h1>qsdqsdqsd</h1>", options).then(pdfBuffer => {
-            return pdfBuffer
-        }).catch(error => {
-            console.log(error);
-        })
+        return generate(html)
     })
     .listen(PORT, () => console.log(`Listening on ${PORT}`))
